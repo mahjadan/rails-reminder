f915e6df6e10c2e305484ef1ba43433aa86cc710
MOHAMOUD.ALJADAN <mahmoudjadan@gmail.com>
2023-04-03 09:29:25 -0300

    refactor: validates due_date exsits

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,7 +1,8 @@
 class Reminder < ApplicationRecord
     validates :title, presence: true
+    validates :due_date, presence: true
     validate :due_date_cannot_be_in_the_past
-    enum :repeat_frequency, { no_repeat:0, daily: 1, weekly: 2, monthly: 3, yearly: 4}
+    enum :repeat_frequency, { no_repeat:0, daily: 1, weekly: 2, monthly: 3, yearly: 4}, default: 0
 
     def due_date_cannot_be_in_the_past
         if due_date.present? && due_date < DateTime.now
