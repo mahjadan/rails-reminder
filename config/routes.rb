529e481edab87a0a6d1d2863781135060ffa3a9a
USER=mohamoud.aljadan <USER=mohamoud.aljadan@rdstation.com>
2023-04-18 16:41:14 -0300

    refactor: change snooze route to get

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :reminders do
     member {
       post  'complete'
-      post 'snooze'
+      get 'snooze'
      }
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
