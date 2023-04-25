28cf0b0d2def29d95befc67e1e754074ebcd5b6e
USER=mohamoud.aljadan <USER=mohamoud.aljadan@rdstation.com>
2023-04-25 11:49:31 -0300

    refactor: move turbo from controller to erb

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,6 +1,6 @@
 class RemindersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_reminder, only: %i[ show edit update destroy snooze ]
+  before_action :set_reminder, only: %i[show edit update destroy snooze]
 
   # GET /reminders or /reminders.json
   def index
@@ -13,18 +13,16 @@ class RemindersController < ApplicationController
   end
 
   # GET /reminders/1 or /reminders/1.json
-  def show
-  end
+  def show; end
 
   # GET /reminders/new
   def new
-    @reminder = Reminder.new(due_date: DateTime.now + 10.minutes )
+    @reminder = Reminder.new(due_date: DateTime.now + 10.minutes)
     @reminder.user = current_user
   end
 
   # GET /reminders/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /reminders or /reminders.json
   def create
@@ -34,14 +32,14 @@ class RemindersController < ApplicationController
     respond_to do |format|
       if @reminder.save
         ReminderJob.perform_async(@reminder.id)
-        @reminder.due_date.to_date == Date.today ? "today-reminders" : "tomorrow-reminders"
-        if @reminder.due_date.to_date == Date.today
-          @target = "today-reminders"
-        elsif @reminder.due_date.to_date == Date.tomorrow
-          @target = "tomorrow-reminders"
-        else
-          @target = "upcoming-reminders"
-        end
+        @reminder.due_date.to_date == Date.today ? 'today-reminders' : 'tomorrow-reminders'
+        @target = if @reminder.due_date.to_date == Date.today
+                    'today-reminders'
+                  elsif @reminder.due_date.to_date == Date.tomorrow
+                    'tomorrow-reminders'
+                  else
+                    'upcoming-reminders'
+                  end
         format.turbo_stream
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -52,25 +50,37 @@ class RemindersController < ApplicationController
   # PATCH/PUT /reminders/1 or /reminders/1.json
   def update
     @reminder.assign_attributes(reminder_params)
-    if @reminder.due_date_changed? || @reminder.repeat_frequency_changed?
-      # delete and recreate just like the update_snooze process 
+    @replace = false
 
-      
-      puts "delte the createeeeeeeeee"
-    else
-      # update
-      puts "updatingggggggggggggggg"
-    end
-    puts "update PARAMS: #{reminder_params}"
-    puts "title changed: #{@reminder.title_changed?}"
-    puts "title changed: #{@reminder.title_change}"
     respond_to do |format|
-      if @reminder.update(reminder_params)
-        format.turbo_stream
-
+      if @reminder.due_date_changed? || @reminder.repeat_frequency_changed?
+        # delete and recreate just like the update_snooze process
+        @old_reminder = @reminder
+        new_due_date = @reminder.due_date
+        @reminder = Reminder.new(@old_reminder.attributes.except('id').merge(
+                                   due_date: new_due_date,
+                                   repeat_frequency: @reminder.repeat_frequency
+                                 ))
+        @reminder.user = current_user
+        if @reminder.save
+          @old_reminder.destroy
+          ReminderJob.perform_async(@reminder.id)
+          @replace = true
+          format.turbo_stream
+          format.html { redirect_to reminder_url(@reminder), notice: "Reminder was successfully created." }
+          format.html { redirect_to reminders_path , notice: "Reminder was successfully created." }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @reminder.errors, status: :unprocessable_entity }
+        end
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @reminder.errors, status: :unprocessable_entity }
+        # update
+        if @reminder.update(reminder_params)
+          format.turbo_stream
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @reminder.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -80,7 +90,7 @@ class RemindersController < ApplicationController
     @reminder.destroy
 
     respond_to do |format|
-      format.html { redirect_to reminders_url, notice: "Reminder was successfully destroyed." }
+      format.html { redirect_to reminders_url, notice: 'Reminder was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -97,7 +107,7 @@ class RemindersController < ApplicationController
         render turbo_stream: turbo_stream.remove(
           @reminder
         ) + turbo_stream.remove(
-          helpers.dom_id(@reminder,:notification)# here only replace the reminder no the whole list
+          helpers.dom_id(@reminder, :notification) # here only replace the reminder no the whole list
         )
       end
     end
@@ -108,34 +118,24 @@ class RemindersController < ApplicationController
     # already using set_reminder
   end
 
-    # POST /reminders/1/update_snooze
+  # POST /reminders/1/update_snooze
   def update_snooze
     puts "SNOOZE POST params #{params}"
     puts "mintues: #{params[:minutes].to_i}"
-    old_reminder = Reminder.find(params[:id])
+    @old_reminder = Reminder.find(params[:id])
     new_due_date = DateTime.now + params[:minutes].to_i.minutes
     puts "new_due_date: #{new_due_date}"
     # check if the new_due_date is still older than now
-    # or render unprocessable_entity but with 
-    @reminder = Reminder.new(old_reminder.attributes.except("id").merge(due_date: new_due_date))
+    # or render unprocessable_entity but with
+    @reminder = Reminder.new(@old_reminder.attributes.except('id').merge(due_date: new_due_date))
     respond_to do |format|
       if @reminder.save
         puts "id: #{@reminder.id}, due_date: #{@reminder.due_date}, title: #{@reminder.title}"
-        old_reminder.destroy
+        @old_reminder.destroy
         ReminderJob.perform_async(@reminder.id)
-        # format.html { redirect_to reminder_url(@reminder), notice: "Reminder was successfully created." }
-        # format.html { redirect_to reminders_path , notice: "Reminder was successfully created." }
-        format.turbo_stream do
-          # replace the new reminder in the reminders list
-          # render turbo_stream: []
-          render turbo_stream: [
-            # turbo_stream.prepend('reminders', partial: "reminders/reminder",locals: {reminder: @reminder}),
-            # remove the old reminder from notification
-            # turbo_stream.remove(old_reminder),
-            turbo_stream.remove(helpers.dom_id(old_reminder,:notification)),
-            turbo_stream.replace(old_reminder, partial: "reminders/reminder",locals: {reminder: @reminder})
-          ]
-        end
+        format.turbo_stream
+        format.html { redirect_to reminders_path , notice: "Reminder was successfully created." }
+        format.html { redirect_to reminder_url(@reminder), notice: "Reminder was successfully created." }
       else
         format.html { render :snooze, status: :unprocessable_entity }
       end
@@ -143,17 +143,16 @@ class RemindersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_reminder
-      begin
-        @reminder = Reminder.find(params[:id])
-      rescue => e
-        redirect_to reminders_path
-      end
-    end
 
-    # Only allow a list of trusted parameters through.
-    def reminder_params
-      params.require(:reminder).permit(:title, :description, :due_date, :repeat_frequency)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_reminder
+    @reminder = Reminder.find(params[:id])
+  rescue StandardError => e
+    redirect_to reminders_path
+  end
+
+  # Only allow a list of trusted parameters through.
+  def reminder_params
+    params.require(:reminder).permit(:title, :description, :due_date, :repeat_frequency)
+  end
 end
