73ec9f98c9da3ff3d92fb632d3caf90c729ad07b
USER=mohamoud.aljadan <USER=mohamoud.aljadan@rdstation.com>
2023-05-15 15:31:33 -0300

    refactor: import matcha in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'support/factory_bot.rb'
+require 'shoulda/matchers'
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
