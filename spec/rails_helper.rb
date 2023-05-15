875872fc8c3452ae8d0a7352780455233da29b49
USER=mohamoud.aljadan <USER=mohamoud.aljadan@rdstation.com>
2023-05-15 15:39:27 -0300

    refactor: fix shoulda-matcher gem config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,19 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'support/factory_bot.rb'
-require 'shoulda/matchers'
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
