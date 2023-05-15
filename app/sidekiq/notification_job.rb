354a0e666342430af2729e78f7966eab4058ea2d
USER=mohamoud.aljadan <USER=mohamoud.aljadan@rdstation.com>
2023-05-15 20:13:12 -0300

    test: add test for the notification job

--- app/sidekiq/notification_job.rb
+++ app/sidekiq/notification_job.rb
@@ -5,8 +5,6 @@ class NotificationJob
     puts "run notification_job with args: ' + #{notification_id}"
     notification = Notification.find_by(id: notification_id)
     if notification.present?
-      # check if the notification has been updated/done
-      # for now let's send notifcation
       send_user_notification(notification)
     end
   end
@@ -14,7 +12,6 @@ class NotificationJob
   private
 
   def send_user_notification(notification)
-    puts "calling notify_user : #{notification}"
     puts "notifying to this channel : notification_channel_#{notification.user.id}"
     # broadcast message to the NotificationChannel check -> app/channels/notification_channel.rb
     ActionCable.server.broadcast "notification_channel_#{notification.user.id}", { notification: notification}
